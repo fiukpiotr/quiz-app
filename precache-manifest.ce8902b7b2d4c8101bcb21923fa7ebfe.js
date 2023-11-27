8b1ab884161c967c5b91651c57ac3083ed549691
Pete Fiuk <piotrek.fiuk@gmail.com>
2023-11-27 11:58:16 +0100

    Updates

---
+++ precache-manifest.ce8902b7b2d4c8101bcb21923fa7ebfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d8da799cfd230a3a627caaa3154216",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "b9a06d5938f5867873be",
+    "url": "/quiz-app/static/css/main.6a63f784.chunk.css"
+  },
+  {
+    "revision": "df83a52f0e98f93779b9",
+    "url": "/quiz-app/static/js/2.e73528b7.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/quiz-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "b9a06d5938f5867873be",
+    "url": "/quiz-app/static/js/main.c4a3c569.chunk.js"
+  },
+  {
+    "revision": "e82ee30bc81aac85da42",
+    "url": "/quiz-app/static/js/runtime~main.994e2a24.js"
+  },
+  {
+    "revision": "297606835ecb560b6360666b43a947b2",
+    "url": "/quiz-app/static/media/fruity-pattern.29760683.jpg"
+  }
+]);
